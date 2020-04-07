f286677f25c6bf8abb71ab7224276ddb4181bc57
hchX009 <hecenhao@outlook.com>
2020-04-07 20:52:01 +0800

    commit 通过使用比较文件创建时间和修改时间解决了没有exif时间信息的情况 in 2020.04.07

--- ChangePicsNameByTime/ChangePicsNameByTime.py
+++ ChangePicsNameByTime/ChangePicsNameByTime.py
@@ -2,70 +2,83 @@ import os
 import time
 import exifread
 
-# 得到exif信息
-def getExif(full_filename, format):
+
+# 得到图片最早的时间信息函数
+def get_first_time(full_filename):
     # 以二进制的方式读取照片文件
     fd = open(full_filename, 'rb')
-    # 提取元信息exif到字典tags中
+
+    # 尝试提取元信息exif到字典tags中
     try:
         tags = exifread.process_file(fd)
     except KeyError:
-        t = os.path.getctime(full_filename)
-        print(full_filename + ' => ' + time.strftime("%Y%m%d_%H%M%S", time.localtime(t)))
-        return
-    # 关闭文件
-    fd.close()
+        # 判断创建时间与修改时间哪一个更早
+        t = get_early_time(os.path.getmtime(full_filename), os.path.getctime(full_filename))
+        time_str = time.strftime("%Y%m%d_%H%M%S", time.localtime(t))
+        # print(full_filename + "未得到Exif信息！")
+        print(full_filename + ' => ' + time_str)
+        return time_str
+    else:
+        # 正常提取exif则关闭文件
+        fd.close()
+
     # 显示图片所有的exif信息
     # print("Show all Exif_info of " + full_filename + " :")
     # print(tags)
-    # 设置只查找DateTimeOriginal的项
-    DTO = 'EXIF DateTimeOriginal'
-    if DTO in tags:
-        # 获取到的结果格式类似为：2018:12:07 03:10:34
-        # print(str(tags[DTO]))
-        # 获取结果格式类似为：20181207_031034
-        # print(str(tags[DTO]).replace(':', '').replace(' ', '_'))
-        name = 'IMG_' + str(tags[DTO]).replace(':', '').replace(' ', '_') + format
-        print(full_filename + ' => ' + name)
-    '''
-        time = new_name.split(".")[0][:13]
-        new_name2 = new_name.split(".")[0][:8] + '_' + filename
-        print("\nfilename: %s" % filename)
-        print("\n%s的拍摄时间是: %s年%s月%s日%s时%s分" % (filename, time[0:4], time[4:6], time[6:8], time[9:11], time[11:13]))
 
-        # 可对图片进行重命名
-        # new_full_file_name = os.path.join(imgpath, new_name2)
-        # print(old_full_file_name," ---> ", new_full_file_name)
-        # os.rename(old_full_file_name, new_full_file_name)
+    # 在元数据exif寻找"EXIF DateTimeOriginal"项，获得拍摄时间
+    if "EXIF DateTimeOriginal" in tags:
+        # 直接获取到的结果格式类似为：2018:12:07 03:10:34
+        # 修改为一定格式的时间信息：20181207_031034
+        time_str = str(tags["EXIF DateTimeOriginal"]).replace(':', '').replace(' ', '_')
+        print(full_filename + ' => ' + time_str)
+        return time_str
     else:
-        print('No {} found'.format(FIELD), ' in: ', old_full_file_name)
-    '''
+        # 如果没有元数据，则返回创建日期或者修改日期中最早的时间
+        # 判断创建时间与修改时间哪一个更早
+        t = get_early_time(os.path.getmtime(full_filename), os.path.getctime(full_filename))
+        time_str = time.strftime("%Y%m%d_%H%M%S", time.localtime(t))
+        # print(full_filename + "未得到Exif信息！")
+        print(full_filename + ' => ' + time_str)
+        return time_str
+
+
+# 从两个时间中获得更早的时间
+def get_early_time(t1, t2):
+    if t1 >= t2:
+        return t2
+    else:
+        return t1
+
+
 # 获得图片格式
-def getFormat(full_filename):
+def get_file_ext(full_filename):
     # 将文件名和扩展名分开 ex：full_filename.txt => full_filename + .txt
-    format = os.path.splitext(full_filename)[1]
+    file_ext = os.path.splitext(full_filename)[1]
+
     # 打印格式
-    # print('文件格式：' + format)
-    return format
+    # print('文件格式：' + file_ext)
+
+    return file_ext
+
 
 # 主函数
 def main():
-    imgFolderPath = "C:/Users/hecen/Desktop/Image" #"../tmp/"
+    img_folder_path = "C:/Users/hecen/Desktop/Image/"
+    # img_folder_path = "I:\BackupPictures\hch14"
 
     # os.listdir() 方法用于返回指定的文件夹包含的文件或文件夹的名字的列表
-    for filename in os.listdir(imgFolderPath):
+    for filename in os.listdir(img_folder_path):
         # os.path.join用于路径拼接，将imgpath和filename连在一起得到完整的路径，后面的参数可有多个，从第一个以”/”开头的参数开始拼接
-        full_filename = os.path.join(imgFolderPath, filename)
+        full_filename = os.path.join(img_folder_path, filename)
         # 打印路径
         # print('照片路径名称：' + full_file_name)
         # os.path.isfile用于判断路径指向的是否为文件，相类似的os.path.isdir用于判断是否为文件夹
         if os.path.isfile(full_filename):
-            format = getFormat(full_filename);
-            if(format == '.jpg'):
-                getExif(full_filename, format)
+            file_ext = get_file_ext(full_filename)
+            file_create_time = get_first_time(full_filename)
+            # print(file_create_time)
 
 
 if __name__ == '__main__':
     main()
-
-
