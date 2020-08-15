802186758bfae23abca4d287fc2b8a6828854fef
hchX009 <hecenhao@outlook.com>
2020-08-15 22:10:48 +0800

    commit 用信号处理尝试了几个函数 in 2020.08.15

--- GUI/test.py
+++ GUI/test.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from GUI.ui_changeFilesNameWindows import *
 
 from PyQt5.QtCore import QCoreApplication
@@ -9,11 +11,36 @@ class MainWindow(QMainWindow, Ui_mainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.num = randint(1, 100)
+
+    def showMessage(self):
+        guessnumber = int(self.fileRouteLineEdit.text())
+        print(self.num)
+
+        if guessnumber > self.num:
+            QMessageBox.about(self, '看结果', '猜大了!')
+            self.fileRouteLineEdit.setFocus()
+        elif guessnumber < self.num:
+            QMessageBox.about(self, '看结果', '猜小了!')
+            self.fileRouteLineEdit.setFocus()
+        else:
+            QMessageBox.about(self, '看结果', '答对了!进入下一轮!')
+            self.num = randint(1, 100)
+            self.fileRouteLineEdit.clear()
+            self.fileRouteLineEdit.setFocus()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, '确认', '确认退出吗', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.browseButton.clicked.connect(QCoreApplication.instance().quit)
+    mainWindow.editByTimeButton.clicked.connect(mainWindow.showMessage)
     mainWindow.show()
     sys.exit(app.exec_())
